dbb4cf3e949235940a1eecf8e32f4ab6e8e25db6
ykyuuuuka <ykyuuuuka@gmail.com>
2021-01-11 14:07:05 +0900

    appdate

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require('express')
+const handlebars = require('express-handlebars')
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
-});
-
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+const app = express()
+app.engine('hbs', handlebars({ extname: 'hbs' }))
+app.set('view engine', 'hbs')
+app.set('views', `${__dirname}/views`)
+app.get('/home', (req, res) => {
+  res.render('home', { layout: false })
+})
+app.get('/error', (req, res) => {
+  res.render('error', { layout: false })
+})
+app.listen(8080, () => console.log('Running on 8080'))
